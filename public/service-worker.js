932f0824a8b82d1eef9204dfc9c518542abf24ea
Joseph Lynn <josephlynn94@gmail.com>
2021-05-25 07:42:17 -0400

    updated npm packages, remformatted code, building out indexedDB functionality

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,7 @@ const FILES_TO_CACHE = [
   '/',
   '/index.html',
   '/styles.css',
+  '/db.js',
   '/dist/manifest.json',
   '/dist/bundle.js',
   '/dist/icon_72x72.png',
@@ -14,51 +15,51 @@ const FILES_TO_CACHE = [
   '/dist/icon_512x512.png',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
-]
+];
 
-const STATIC_CACHE = "static-cache-v1";
-const RUNTIME_CACHE = "runtime-cache";
+const STATIC_CACHE = 'static-cache-v1';
+const RUNTIME_CACHE = 'runtime-cache';
 
 // install to perform install steps
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
   // waitUntil method takes a promise and uses it to know how long installation takes, and whether it succeeded or not.
   event.waitUntil(
-    caches 
+    caches
       // Open a cache
       .open(STATIC_CACHE)
       // Cache our files
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
       // skipWaiting will only have an effect if there's a newly installed service worker that might otherwise remain in the waiting state
       .then(() => self.skipWaiting())
-  )
-})
+  );
+});
 
-// activate to make sure everything is up to date 
-self.addEventListener("activate", (event) => {
+// activate to make sure everything is up to date
+self.addEventListener('activate', (event) => {
   const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
 
   event.waitUntil(
-    caches  
-      // get keys to all of our caches 
+    caches
+      // get keys to all of our caches
       .keys()
-      .then(cacheNames => {
-        // look through all caches availble to service worker 
-        // filter cache names - if it does not include a defined cache name, it will be dropped 
-        return cacheNames.filter(name => !currentCaches.includes(name))
+      .then((cacheNames) => {
+        // look through all caches availble to service worker
+        // filter cache names - if it does not include a defined cache name, it will be dropped
+        return cacheNames.filter((name) => !currentCaches.includes(name));
       })
-      .then(cachesToDelete => {
+      .then((cachesToDelete) => {
         // return array of cache names that are old to delete
-        return Promise.all(cachesToDelete.map(name => caches.delete(name)))
+        return Promise.all(cachesToDelete.map((name) => caches.delete(name)));
       })
       // allows an active service worker to set itself as the controller for all clients within its scope
       .then(() => self.clients.claim())
-  )
-})
+  );
+});
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   // non GET requests are not cached and requests to other origins are not cached
   if (
-    event.request.method !== "GET" ||
+    event.request.method !== 'GET' ||
     !event.request.url.startsWith(self.location.origin)
   ) {
     event.respondWith(fetch(event.request));
@@ -66,42 +67,37 @@ self.addEventListener("fetch", (event) => {
   }
 
   // handle runtime GET requests for data from /api routes
-  if(event.request.url.includes("/api/")) {
+  if (event.request.url.includes('/api/')) {
     // make network request and fallback to cache if network request fails (offline)
     event.respondWith(
-      caches
-        .open(RUNTIME_CACHE)
-        .then(cache => {
-          return fetch(event.request)
-            .then(response => {
+      caches.open(RUNTIME_CACHE).then((cache) => {
+        return (
+          fetch(event.request)
+            .then((response) => {
               // If the response was good, clone it and store it in the cache.
               cache.put(event.request, response.clone());
 
               return response;
             })
             // Network request failed, try to get it from the cache.
-            .catch(() => caches.match(event.request));
-        })
-      );
-      
+            .catch(() => caches.match(event.request))
+        );
+      })
+    );
+
     return;
   }
 
   // use cache first for all other requests for performance
   event.respondWith(
-    caches.match(event.request)
-      .then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse
-        }
-        // request is not in cache. make network request and cache the response
-        caches
-          .open(RUNTIME_CACHE)
-          .then(response => {
-            return response || fetch(event.request)
-          })
-      })
-  )
-
-})
-
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      // request is not in cache. make network request and cache the response
+      caches.open(RUNTIME_CACHE).then((response) => {
+        return response || fetch(event.request);
+      });
+    })
+  );
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
-const path = require("path");
-const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require('path');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 const config = {
-  entry: "./public/index.js",
-  output: {
-    path: path.join(__dirname, "public", "dist"),
-    filename: "bundle.js"
+  entry: {
+    app: './public/index.js',
+    db: './public/db.js',
   },
-  mode: "production",
+  output: {
+    path: path.join(__dirname, 'public/dist/'),
+    filename: '[name].bundle.js',
+  },
+  mode: 'production',
   plugins: [
     new WebpackPwaManifest({
       // the name of the generated manifest file
-      filename: "manifest.json",
+      filename: 'manifest.json',
 
       // we aren't using webpack to generate our html so we
       // set inject to false
@@ -21,20 +24,23 @@ const config = {
       // files predictable making it easier to refer to them in our code
       fingerprints: false,
 
-      name: "Budget Tracker",
-      short_name: "Budget Tracker",
-      theme_color: "#ffffff",
-      background_color: "#ffffff",
-      start_url: "/",
-      display: "standalone",
+      name: 'Budget Tracker',
+      short_name: 'Budget Tracker',
+      description:
+        'An application that allows you to add expenses and deposits to a budget.',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      start_url: '/',
+      display: 'standalone',
 
       icons: [
         {
-          src: path.resolve(__dirname, "public/icons/icon-192x192.png"),
+          src: path.resolve(__dirname, 'public/icons/icon-192x192.png'),
           // the plugin will generate an image for each size
           // included in the size array
-          size: [72, 96, 128, 144, 152, 192, 384, 512]
-        }
+          size: [72, 96, 128, 144, 152, 192, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
       ],
       module: {
         rules: [
@@ -42,16 +48,16 @@ const config = {
             test: /\.js$/,
             exclude: /node_modules/,
             use: {
-              loader: "babel-loader",
+              loader: 'babel-loader',
               options: {
-                presets: ["@babel/preset-env"]
-              }
-            }
-          }
-        ]
-      }
-    })
-  ]
-}
+                presets: ['@babel/preset-env'],
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+};
 
-module.exports = config
+module.exports = config;
